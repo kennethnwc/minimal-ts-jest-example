f7a64011fe634c5ebf5390da2d4110a8b8f04394
Wing chung Ng <wingchungng@wings-mbp.home>
2023-08-23 20:24:40 -0400

    init

---
+++ src/index.test.ts
@@ -0,0 +1,21 @@
+import { stringToNumber, sum } from ".";
+
+beforeEach(() => {
+  console.log("before test");
+});
+
+afterEach(() => {
+  console.log("after test");
+});
+
+test("1 + 1", () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+test("2 + 3", () => {
+  expect(sum(2, 3)).toBe(5);
+});
+
+it("string to number", () => {
+  expect(stringToNumber("123")).toBe(123);
+});
